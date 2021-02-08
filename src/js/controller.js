a07271c150d09be3a932ba8fae4ac88990947142
Stas Lifanov <holidayonmail@gmail.com>
2021-02-09 00:50:42 +0300

    Added links to sections

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,6 @@
 import icons from "url:../img/icons.svg";
+import "core-js/stable";
+import "regenerator-runtime";
 
 console.log(icons);
 
@@ -16,8 +18,22 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+const renderSpinner = function (parentEl) {
+  const markup = `
+  <div class="spinner">
+          <svg>
+            <use href="${icons}#icon-loader"></use>
+          </svg>
+        </div>
+  `;
+
+  parentEl.innerHTML = "";
+  parentEl.insertAdjacentHTML("afterbegin", markup);
+};
+
 const showRecipe = async function () {
   try {
+    renderSpinner(recipeContainer);
     const res = await fetch(
       "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
     );
